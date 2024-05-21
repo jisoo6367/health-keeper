690fd0ee458bd7569016f98d31c896b738dc2826
jisoo6367 <jisunala@hanmail.net>
2024-05-21 22:38:11 +0900

    0521 lesson table

---
+++ src/main/java/com/health/keeper/entity/LessonEntity.java
@@ -0,0 +1,57 @@
+package com.health.keeper.entity;
+
+import com.health.keeper.dto.MenuDTO;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+// DB 테이블 역할을 하는 클래스
+@Entity
+@Getter
+@Setter
+@Table(name = "lesson_table")
+public class LessonEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //예약 고유넘버
+
+    @Column
+    private Long classId; //수업 고유아이디
+
+    @Column(length = 500)
+    private String className;
+
+    @Column
+    private Long instructorId; //강사 아이디
+
+    @Column
+    @Temporal(TemporalType.DATE) // Date 타입으로 매핑
+    private LocalDate reservationDate; //예약날짜
+
+    @Column
+    @Temporal(TemporalType.DATE)
+    private LocalDate startTime;//수업 시작시간
+
+    @Column
+    @Temporal(TemporalType.DATE)
+    private LocalDate endTime; //수업 종료시간
+
+    @Column(length = 500)
+    private String Status; //예약상태
+
+    // JPA 에서 제공하는 어노테이션
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @ManyToOne(fetch = FetchType.LAZY) //부모 엔티티 조회시 필요한 상황에만 자식엔티티 같이 조회할 수 있음, EAGER는 항상 같이 가져옴
+    @JoinColumn(name = "user_id") // DB에 만들어지는 컬럼 이름
+    private UserEntity userEntity; // 반드시 부모 엔티티 타입으로 적어줘야함
+
+
+
+}
